29824d4641f32478c015098799d0cf8c4b138bc7
0xJoroh <mohamedaitsimhand@gmail.com>
2021-10-26 16:40:56 +0100

    in_redirections

--- minishell.c
+++ minishell.c
@@ -11,13 +11,13 @@ t_list	*get_args(t_list **args ,t_type	*types)
 	while (tmp)
 	{
 		prev = tmp->prev;
-		if (tmp->type == 4)
+		if (tmp->type == 4 || tmp->type == 3)
 			ft_lstadd_back(&list_files, ft_lstnew(tmp->next->word));
-		if (tmp->type != 4)
+		if (tmp->type != 4 && tmp->type != 3)
 		{
 			if (tmp->prev != NULL)
 			{
-				if (tmp->prev->type != 4)
+				if (tmp->prev->type != 4 && tmp->prev->type != 3)
 					help_args(&tmp, args);
 			}
 			else
@@ -25,16 +25,16 @@ t_list	*get_args(t_list **args ,t_type	*types)
 		}
 		tmp = tmp->next;
 	}
-	t_list *list = *args;
-	while (list)
-	{
-		printf("tt = %s\n", (char *)(list)->content);
-		list = (list)->next;
-	}
+	// t_list *list = *args;
+	// while (list)
+	// {
+	// 	printf("tt = %s\n", (char *)(list)->content);
+	// 	list = (list)->next;
+	// }
 	return (list_files);
 }
 
-void	get_out(int *i, t_list *list_files)
+void	get_out(int *i, t_list *list_files, t_type *expanded_types)
 {
 	char	*s;
 	size_t	a;
@@ -45,7 +45,12 @@ void	get_out(int *i, t_list *list_files)
 		printf("file_out is = %s\n", s);
 		a = ft_strlen(list_files->content);
 		j = ft_strlen(s);
-		*i = open(s, O_WRONLY | O_CREAT | O_TRUNC , 0777);
+		while (strcmp(expanded_types->word, s) != 0)
+			expanded_types = expanded_types->next;
+		if (expanded_types->prev->type == 4)
+			*i = open(s, O_WRONLY | O_CREAT | O_TRUNC , 0777);
+		else
+			*i = open(s, O_WRONLY | O_CREAT | O_APPEND , 0777);
 		while (list_files)
 		{
 			if (ft_strcmp(s, list_files->content) != 0 || a != j)
@@ -63,7 +68,7 @@ void	first_round(t_type *tmp2, char *str, t_cmd **cmd, t_type **expanded_types)
 	t_type	*tmp;
 
 	tmp = tmp2;
-	if (tmp2->type == 4)
+	if (tmp2->type == 4 || tmp2->type == 3)
 	{
 		if (ft_lstsize_type(tmp2) == 3)
 		{
@@ -72,7 +77,7 @@ void	first_round(t_type *tmp2, char *str, t_cmd **cmd, t_type **expanded_types)
 		}
 		else
 		{
-			while(tmp->type == 4)
+			while(tmp->type == 4 || tmp->type == 3)
 				tmp = tmp->next->next;
 			(*cmd)->cmd = get_cmd_path(tmp->word, g_data->env);
 		}
@@ -99,7 +104,7 @@ void	expand_cmdlist(int i, t_list *tmp, char *str)
 			cmd->cmd = get_cmd_path(expanded_types->word, g_data->env);
 		cmd->args_list = NULL;
 		list_files = get_args(&(cmd->args_list), expanded_types);
-		get_out(&(cmd->out), list_files);
+		get_out(&(cmd->out), list_files, expanded_types);
 		cmd->in = 1;
 		ft_lstadd_back(&g_data->cmd_list, ft_lstnew(cmd));
 		tmp = tmp->next;
